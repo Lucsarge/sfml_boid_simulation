3da7fe6be80154832da14735a112a6342f5d13b6
Luc Sargent <lucsarge@gmail.com>
2024-09-30 11:23:19 -0800

    learning: fundamental memory address debugging

--- SFMLBoidSimulation/boid.cpp
+++ SFMLBoidSimulation/boid.cpp
@@ -1,28 +1,14 @@
 #include "boid.hpp"
 
+
 namespace boid_sim {
-	Boid::Boid() {
-		boidPos = sf::Vector2f(0.f, 0.f);
-
-		sf::ConvexShape arrowHead = sf::ConvexShape(3);
-		arrowHead.setPoint(0, sf::Vector2f(0.f, -1.f));
-		arrowHead.setPoint(1, sf::Vector2f(0.5f, 0.5f));
-		arrowHead.setPoint(2, sf::Vector2f(-0.5f, 0.5f));
-		arrowHead.setFillColor(sf::Color::Transparent);
-		arrowHead.setOutlineColor(sf::Color::Green);
-		arrowHead.setOutlineThickness(0.1f);
-		boidShape = arrowHead;
-	}
-
-	Boid::Boid(sf::Vector2f position, float rotation, sf::ConvexShape shape) {
-		boidShape = shape;
-
+	Boid::Boid(sf::Vector2f position, float rotation, sf::ConvexShape& shape) : boidShape{shape} {
 		updatePos(position);
 		updateRot(rotation);
 	}
 
 	void Boid::updatePos(sf::Vector2f newPos) {
-		boidPos = newPos;
+		boidPos += newPos;
 		boidShape.setPosition(boidPos);
 	}
 
